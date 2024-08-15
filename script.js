eb2bf6d35aafb5e5aff0da67403439620fe3e345
Bulent Buyukkahraman <bulent@buyukkahraman.com>
2024-08-15 19:50:09 +0300

    JavaScript eklenerek site etkileşimli yapildi

---
+++ script.js
@@ -0,0 +1,9 @@
+// Butona tıklandığında mesaj göster
+function showMessage() {
+    alert('Merhaba! Bu bir etkileşimli mesajdır.');
+}
+
+// Sayfa yüklendiğinde bir mesaj göster
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Sayfa yüklendi.');
+});
